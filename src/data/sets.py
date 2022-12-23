62889ca39fdd7a823635eb2ef620dc6f6e2a1622
Amy <amyyangsyd2014@gmial.com>
2022-12-23 14:28:04 +1100

    update the notebook for neural network model

--- src/data/sets.py
+++ src/data/sets.py
@@ -106,3 +106,16 @@ def save_sets(X_train=None, y_train=None, X_val=None, y_val=None, X_test=None, y
       np.save(f'{path}y_val', y_val)
     if y_test is not None:
       np.save(f'{path}y_test', y_test)
+
+def load_sets(path='../data/processed/', val=False):
+    import numpy as np
+    import os.path
+
+    X_train = np.load(f'{path}X_train.npy') if os.path.isfile(f'{path}X_train.npy') else None
+    X_val   = np.load(f'{path}X_val.npy'  ) if os.path.isfile(f'{path}X_val.npy')   else None
+    X_test  = np.load(f'{path}X_test.npy' ) if os.path.isfile(f'{path}X_test.npy')  else None
+    y_train = np.load(f'{path}y_train.npy') if os.path.isfile(f'{path}y_train.npy') else None
+    y_val   = np.load(f'{path}y_val.npy'  ) if os.path.isfile(f'{path}y_val.npy')   else None
+    y_test  = np.load(f'{path}y_test.npy' ) if os.path.isfile(f'{path}y_test.npy')  else None
+    
+    return X_train, y_train, X_val, y_val, X_test, y_test
